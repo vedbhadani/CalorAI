8f0a6363e925b4968215bde94181cc01d397178d
vedbhadani <vedbhadani157@gmail.com>
2026-04-24 17:58:33 +0530

    feat(core): setup App routing and context providers

---
+++ src/App.tsx
@@ -0,0 +1,26 @@
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { AnimatePresence } from 'framer-motion';
+import { ThemeProvider } from './context/ThemeContext';
+import { SwipeProvider } from './context/SwipeContext';
+import { IntroPage }   from './pages/IntroPage';
+import { SwipePage }   from './pages/SwipePage';
+import { ResultsPage } from './pages/ResultsPage';
+
+export default function App() {
+  return (
+    <ThemeProvider>
+      <SwipeProvider>
+        <BrowserRouter>
+          <AnimatePresence mode="wait">
+            <Routes>
+              <Route path="/"        element={<IntroPage />} />
+              <Route path="/swipe"   element={<SwipePage />} />
+              <Route path="/results" element={<ResultsPage />} />
+              <Route path="*"        element={<Navigate to="/" />} />
+            </Routes>
+          </AnimatePresence>
+        </BrowserRouter>
+      </SwipeProvider>
+    </ThemeProvider>
+  );
+}
