2f31a0225e941c992986514a1f3d8d17187a0b4b
vedbhadani <vedbhadani157@gmail.com>
2026-04-25 00:24:24 +0530

    fix(layout): fix page transitions and disable global overscroll bounce effect

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Navigate, useLocation } from 'react-router-dom';
 import { AnimatePresence } from 'framer-motion';
 import { ThemeProvider } from './context/ThemeContext';
 import { SwipeProvider } from './context/SwipeContext';
@@ -7,20 +7,27 @@ import { SwipePage }   from './pages/SwipePage';
 import { ResultsPage } from './pages/ResultsPage';
 import { AnimatedBackground } from './components/ui/AnimatedBackground';
 
+const AnimatedRoutes = () => {
+  const location = useLocation();
+  return (
+    <AnimatePresence mode="wait">
+      <Routes location={location} key={location.pathname}>
+        <Route path="/"        element={<IntroPage />} />
+        <Route path="/swipe"   element={<SwipePage />} />
+        <Route path="/results" element={<ResultsPage />} />
+        <Route path="*"        element={<Navigate to="/" />} />
+      </Routes>
+    </AnimatePresence>
+  );
+};
+
 export default function App() {
   return (
     <ThemeProvider>
       <SwipeProvider>
         <AnimatedBackground />
         <BrowserRouter>
-          <AnimatePresence mode="wait">
-            <Routes>
-              <Route path="/"        element={<IntroPage />} />
-              <Route path="/swipe"   element={<SwipePage />} />
-              <Route path="/results" element={<ResultsPage />} />
-              <Route path="*"        element={<Navigate to="/" />} />
-            </Routes>
-          </AnimatePresence>
+          <AnimatedRoutes />
         </BrowserRouter>
       </SwipeProvider>
     </ThemeProvider>
